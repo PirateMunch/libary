4a6d6df7d432fd18be642fc317717902ea828ea7
neal <nealwilson@live.co.uk>
2022-07-04 18:48:03 +0100

    book constructor and submit button working

--- script.js
+++ script.js
@@ -1,43 +1,55 @@
 //get user info
-let userTitle = document.getElementById("title")
-let userAuthor = document.getElementById("author")
-let userPages = document.getElementById("pages")
-let userPublished = document.getElementById("published")
-let userRead = document.getElementById("read")
-let userStars = document.getElementById("stars")
-let userDateRead = document.getElementById("dateRead")
+const userTitle = document.getElementById("title")
+const userAuthor = document.getElementById("author")
+const userPages = document.getElementById("pages")
+const userPublished = document.getElementById("published")
+const userRead = document.getElementById("read")
+const userDateRead = document.getElementById("dateRead")
+
+
+window.onload = function() {
+  userTitle.addEventListener("form", function() {
+  console.log(userTitle.value);
+})
+}
 
 let myLibrary = [];
 
 // constructor for books
-function Book(title, author, pages, published, read, stars, dateRead) {
+function Book(title, author, pages, published, read, dateRead) {
   this.title = title
   this.author = author
   this.pages = pages
   this.published = published
   this.read = read
-  this.stars = stars
   this.dateRead = dateRead
   this.info = function() {
-    return [title, author, pages, published, read, stars, dateRead]
+    return [title, author, pages, published, read, dateRead]
   }
 }
+
+
 // test book
-const lordOfFlies = new Book("Lord of the Flies", "William Golding", "224", "17 September 1954", true, 3, "11/12/1982")
+const lordOfFlies = new Book("Lord of the Flies", "William Golding", "224", "17 September 1954", true, "11/12/1982")
+const testbook = new Book("test book", "test guy", "1224", "12 September 1954", false, "01/12/1934")
+// const userBook = new Book(userTitle, userAuthor, userPages, userPublished, userRead, userStars, userDateRead)
+  
 
-// make user info = newbook....
- const userBook = new Book(userTitle, userAuthor, userPages, userPublished, userRead, userStars, userDateRead)
-
-// change push info to user.info....
+// change push info to user.info..??
 function addBookToLibrary() {
-  myLibrary.push(userBook.info())
-  // myLibrary.push(lordOfFlies.info())
-  console.log(myLibrary)
+  // myLibrary.push(userBook.info())
+  myLibrary.push(lordOfFlies.info())
+  myLibrary.push(testbook.info())
+  // console.log(myLibrary)
 }
 
-/* function activated by HTML button */
-function createBook() {
-  const userBook = new Book(userTitle, userAuthor, userPages, userPublished, userRead, userStars, userDateRead)
-  addBookToLibrary()
+function testFunc() {
+  console.log(myLibrary)
+  console.log("help")
+  console.log(userTitle.value)
 }
-// submit.addEventListener("click", addBookToLibrary())
+
+// add new book function on button
+const addBtn = document.querySelector("#addNewBook")
+addBtn.addEventListener('click', testFunc())
+ 
