6c5b4347fc2bd516895535f2b26a1884f94d6ea6
neal <nealwilson@live.co.uk>
2022-07-07 15:23:21 +0100

    toggleForm button working. Form button working. update display books working

--- script.js
+++ script.js
@@ -13,22 +13,22 @@ function Book(title, author, pages, published, read, dateRead) {
   }
 };
 
-
 // test book
 const lordOfFlies = new Book("Lord of the Flies", "William Golding", "224", "17 September 1954", "on", "11/12/1982");
 const testbook = new Book("test book", "test guy", "1224", "12 September 1954", "off", "01/12/1934");
 
 function addBookToLibrary() {
-  myLibrary.push(userBook.info())
   myLibrary.push(lordOfFlies.info())
   myLibrary.push(testbook.info())
+  myLibrary.push(userBook.info())
 };
 
 const form = document.getElementById('form');
-
+const getForm = document.getElementById("getForm");
+const bookShelf = document.getElementById("books");
 let userBook;
 
-const test = function (event) {
+const formSubmit = function (event) {
   //this little bugger had me stuck for too long!! im sure it disabled my button before.
   event.preventDefault();
   //declare my values in the function for my eventlistener submit button
@@ -41,8 +41,46 @@ const test = function (event) {
   
   userBook = new Book(title, author, pages, published, read, dateRead);
   addBookToLibrary();
-  
-  console.log(myLibrary);
+  buildLibary();
 };
 
-form.addEventListener("submit", test);
+
+const buildLibary = function() {
+  myLibrary.forEach(renderlist);
+  function renderlist(element, index, arr) {
+    //create list items with class of book and append
+    var li = document.createElement('li');
+    const classAttribute = document.createAttribute("class");
+    classAttribute.value = "book";
+    li.setAttributeNode(classAttribute);
+
+    bookShelf.appendChild(li);
+
+    li.innerHTML = li.innerHTML + element;
+  }
+};
+
+const showLibary = function () {
+  myLibrary.push(lordOfFlies.info())
+  myLibrary.push(testbook.info())
+  buildLibary()
+}
+
+function toggleForm() {
+  const newForm = document.getElementById("hideForm");
+  const displaySetting = newForm.style.display;
+
+  if(displaySetting == 'grid') {
+    newForm.style.display = 'none';
+  }
+  else {
+    newForm.style.display ='grid';
+  }
+}
+
+
+form.addEventListener("submit", formSubmit);
+
+getForm.addEventListener('click', toggleForm);
+
+window.onLoad = showLibary();
