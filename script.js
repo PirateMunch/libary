56bb556397a494b3f76e533b9f94c5fc5e5e11ce
neal <nealwilson@live.co.uk>
2022-07-08 01:46:32 +0100

    tidyed code, lots of notes add'd.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let myLibrary = [];
 
-// constructor for books
+// constructor for books could be a Class.
 function Book(title, author, pages, published, read, dateRead) {
   this.title = title
   this.author = author
@@ -13,20 +13,16 @@ function Book(title, author, pages, published, read, dateRead) {
   }
 };
 
-// test book
+// test - book data for working on project
 const lordOfFlies = new Book("Lord of the Flies", "William Golding", "224", "17 September 1954", "on", "11/12/1982");
 const testbook = new Book("test book", "test guy", "1224", "12 September 1954", "off", "01/12/1934");
 
+//need refined after building
 function addBookToLibrary() {
   myLibrary.push(lordOfFlies.info())
   myLibrary.push(testbook.info())
   myLibrary.push(userBook.info())
 };
-
-const form = document.getElementById('form');
-const getForm = document.getElementById("getForm");
-const bookShelf = document.getElementById("books");
-let userBook;
 
 const formSubmit = function (event) {
   //this little bugger had me stuck for too long!! im sure it disabled my button before.
@@ -40,14 +36,22 @@ const formSubmit = function (event) {
   dateRead = this.dateRead.value;
   
   userBook = new Book(title, author, pages, published, read, dateRead);
-  addBookToLibrary();
-  buildLibary();
+  myLibrary.push(userBook.info());
   toggleForm();
+  //bug -- stop duplicates here
+  buildLibary();
+  
 };
 
 
 function buildLibary() {
   myLibrary.forEach(renderlist);
+// add for if here to exclude doubles. Or before append?
+// for (let i in myLibrary) { 
+//   if(i === --i) {
+//     continue;
+//     } run renderlist.....  
+
   function renderlist(element, index, arr) {
     //create list items with class of book and append
     var li = document.createElement('li');
@@ -57,24 +61,17 @@ function buildLibary() {
 
     bookShelf.appendChild(li);
 
+    //test
     console.log(element[0]);
-// make book into nice list
+// make book into nice list - works, maybe bit crude.
     li.innerHTML = `Title \xa0: \xa0\xa0 ${element[0]}` +"<br>"+ `Author \xa0:\xa0\xa0 ${element[1]}`  +"<br>"+ `Pages \xa0:\xa0\xa0 ${element[2]}`
     +"<br>"+ `Published \xa0:\xa0\xa0  ${element[3]}`  +"<br>"+ `Read \xa0:\xa0\xa0 ${element[4]}`  +"<br>"+ `Finished Reading Book \xa0:\xa0\xa0 ${element[5]}`
   }
 };
 
-
-function showLibary() {
-  myLibrary.push(lordOfFlies.info());
-  myLibrary.push(testbook.info());
-  buildLibary();
-};
-
 function toggleForm() {
   const newForm = document.getElementById("hideForm");
   const displaySetting = newForm.style.display;
-
   if(displaySetting == 'grid') {
     newForm.style.display = 'none';
   }
@@ -83,11 +80,26 @@ function toggleForm() {
   }
 };
 
+//declare globals here
+const form = document.getElementById('form');
+const getForm = document.getElementById("getForm");
+const bookShelf = document.getElementById("books");
+let userBook;
 
+// upload new book button
 form.addEventListener("submit", formSubmit);
 
+//add new book button
 getForm.addEventListener('click', toggleForm);
 
+// test - get display working. need refined 
+function showLibary() {
+  myLibrary.push(lordOfFlies.info());
+  myLibrary.push(testbook.info());
+  buildLibary();
+};
 window.onLoad = showLibary();
+
+
 //bugs -- Submit book returns all of myLibary array concated up
 //would like -- add new book button should disapear when form opens up
