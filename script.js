9a1c096ae9ae669a496487df947476460dde2b02
neal <nealwilson@live.co.uk>
2022-07-09 16:06:40 +0100

    gave each book delete button, data-attribute is its posistion in myLibary array. tried to fix style in large screens to not be broken

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ let myLibrary = [];
 let submitArray = [];
 
 // constructor for books could be a Class.
-function Book(title, author, pages, published, read, dateRead) {
+function Book(title, author, pages, published, read, dateRead, data) {
   this.title = title
   this.author = author
   this.pages = pages
   this.published = published
   this.read = read
-  this.dateRead = dateRead
+  this.data = data
   this.info = function() {
-    return [title, author, pages, published, read, dateRead]
+    return [title, author, pages, published, read, dateRead, data]
   }
 };
 
 // test - book data for working on project
-const lordOfFlies = new Book("Lord of the Flies", "William Golding", "224", "17 September 1954", "on", "11/12/1982");
-const testbook = new Book("test book", "test guy", "1224", "12 September 1954", "off", "01/12/1934");
+const lordOfFlies = new Book("Lord of the Flies", "William Golding", "224", "17 September 1954", "on", "11/12/1982", 0);
+const testbook = new Book("test book", "test guy", "1224", "12 September 1954", "off", "01/12/1934", 1);
 
 //need refined after building
 function addBookToLibrary() {
@@ -35,17 +35,17 @@ const formSubmit = function (event) {
   published = this.published.value;
   read = this.read.value;
   dateRead = this.dateRead.value;
-  
   userBook = new Book(title, author, pages, published, read, dateRead);
   myLibrary.push(userBook.info());
   submitArray.push(userBook.info());
   toggleForm();
   addNewBook();
+  makeNum();
 };
 
 function addNewBook() {
   newBook = myLibrary[myLibrary.length -1];
-  console.log(newBook);
+  indexNum = myLibrary.length;
    //create list items with class of book and append
     var li = document.createElement('li');
     const classAttribute = document.createAttribute("class");
@@ -56,8 +56,13 @@ function addNewBook() {
 // make book into nice list - works, maybe bit crude.
     li.innerHTML = `Title \xa0: \xa0\xa0 ${newBook[0]}` +"<br>"+ `Author \xa0:\xa0\xa0 ${newBook[1]}`  +"<br>"+ `Pages \xa0:\xa0\xa0 ${newBook[2]}`
     +"<br>"+ `Published \xa0:\xa0\xa0  ${newBook[3]}`  +"<br>"+ `Read \xa0:\xa0\xa0 ${newBook[4]}`  +"<br>"+ `Finished Reading Book \xa0:\xa0\xa0 ${newBook[5]}`
+    +"<br>"+ `<button type="button" data-index='${indexNum}' class='delete'>Delete Book</button>`
   }
 
+function makeNum () {
+  const thisthing = document.querySelector('.delete');
+  console.log(thisthing.dataset.index)
+}
 
 function buildLibary() {
   myLibrary.forEach(renderlist);
@@ -73,6 +78,7 @@ function buildLibary() {
 // make book into nice list - works, maybe bit crude.
     li.innerHTML = `Title \xa0: \xa0\xa0 ${element[0]}` +"<br>"+ `Author \xa0:\xa0\xa0 ${element[1]}`  +"<br>"+ `Pages \xa0:\xa0\xa0 ${element[2]}`
     +"<br>"+ `Published \xa0:\xa0\xa0  ${element[3]}`  +"<br>"+ `Read \xa0:\xa0\xa0 ${element[4]}`  +"<br>"+ `Finished Reading Book \xa0:\xa0\xa0 ${element[5]}`
+    +"<br>"+ `<button type="button" data-index='${element[6]}' class='delete'>Delete Book</button>`
   }
 };
 
@@ -89,15 +95,28 @@ function toggleForm() {
   }
 };
 
+
+function test (){
+  console.log('foobar')
+}
+
 //declare globals here
 const form = document.getElementById('form');
 const getForm = document.getElementById("getForm");
 const bookShelf = document.getElementById("books");
+const deleteBook = document.getElementsByClassName("delete");
+for(var i = 0; i < deleteBook.length; i++) {
+  deleteBook[i].addEventListener('click', test);
+};
+
 let userBook;
 // upload new book button
 form.addEventListener("submit", formSubmit);
 //add new book button
 getForm.addEventListener('click', toggleForm);
+
+
+
 
 // test - get display working. need refined 
 function showLibary() {
