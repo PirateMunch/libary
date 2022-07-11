f1b61091a1fbb539f6ce5548b06e7f162df23dcd
neal <nealwilson@live.co.uk>
2022-07-11 21:28:59 +0100

    changed radio buttons to match. No other progress, kinda stuckgit add script.js style.css

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ let myLibrary = [];
 let submitArray = [];
 let userBook;
 
-
 const form = document.getElementById('form');
 const getForm = document.getElementById("getForm");
 const bookShelf = document.getElementById("books");
-const emptyBook = document.getElementById("emptyBook")
+const emptyBook = document.getElementById("emptyBook");
+
+
+const radioButton = document.getElementsByClassName('radioClass');
+const deleteButton = document.querySelectorAll('[data-delete]');
+
+let radioSelected;
+let indexNum2 = 0;
+function uniqueID() {
+  return indexNum2++
+}
 
 // constructor for books could be a Class.
 function Book(title, author, pages, published, read, dateRead, data) {
@@ -24,8 +33,8 @@ function Book(title, author, pages, published, read, dateRead, data) {
 
 //logic to build the display with new books, adding attributes, unique data-index
 function addBookToLibrary() {
-  newBook = myLibrary[myLibrary.length -1];
-  indexNum = myLibrary.length;
+  newBook = submitArray[submitArray.length -1]; //not best way to get newbook. infact is causing issues i think
+  indexNum = uniqueID();
    //create list items with class of book and append
     var li = document.createElement('li');
     const classAttribute = document.createAttribute("class");
@@ -58,10 +67,12 @@ function addBookToLibrary() {
     deleteclass.value ="delete";
     const deletedata = document.createAttribute("data-index");
     deletedata.value = indexNum;
+    button.dataset.delete = "delete";
     button.setAttributeNode(deletedata);
     button.setAttributeNode(deleteclass);
     const deleteImg = '<img src="delete1.svg">';
     button.innerHTML = deleteImg;
+
     box.appendChild(button)
 
     //create event listener with the dom element.
@@ -71,76 +82,101 @@ function addBookToLibrary() {
         //remove book from display
         var boss = this.parentNode
         var bigBoss = boss.parentNode
+        console.log(element.value)
         bigBoss.remove();
-        //remove book from myLibary array
-          numPop = element.dataset.index - 1;
-          console.log(numPop)
-          myLibrary.splice(numPop, 1)
+        //remove book from myLibary array 
+          //needs fix need delete button caches... for loop better maybe
           buildLibary();
       })
     });
 
-        //read status.  not sure i like 4 extra boxes here, but works
+        //read status.  not sure i like 4 extra boxes here, but work       
+      var para = document.createElement('p');
+      const paraClass = document.createAttribute("class");
+      paraClass.value = "paraBox";
+      para.setAttributeNode(paraClass);
+      para.innerText = "Have you read this book?"
+      box.appendChild(para);   
+        
+        var readYes = document.createElement('input');
+        const yesClass = document.createAttribute("class");
+        yesClass.value = "radioClass";
+        readYes.setAttributeNode(yesClass);
+        const yesType = document.createAttribute("type");
+        yesType.value = "radio";
+        readYes.setAttributeNode(yesType);
+        const yesName = document.createAttribute("name");
+        yesName.value = "readSelect";
+        readYes.setAttributeNode(yesName);
+        const yesValue = document.createAttribute("value");
+        yesValue.value = "Yes";
+        readYes.setAttributeNode(yesValue);
+        readYes.dataset.radio = "yes";
+        readYes.innerHTML = "yes";
+        para.appendChild(readYes);
+
+        var readlabelYes = document.createElement('label');  
+        const labelclassYes = document.createAttribute("class");
+        labelclassYes.value = "yesClass";
+        readlabelYes.setAttributeNode(labelclassYes);
+        const labelForYes = document.createAttribute("for");
+        labelForYes.value = "readSelect";
+        readlabelYes.setAttributeNode(labelForYes);
+        readlabelYes.innerHTML = "Yes";
+        para.appendChild(readlabelYes);
+ 
+        var readNo = document.createElement('input');
+        const noClass = document.createAttribute("class");
+        noClass.value = "radioClass";
+        readNo.setAttributeNode(noClass);
+        const noType = document.createAttribute("type");
+        noType.value = "radio";
+        readNo.setAttributeNode(noType);
+        const noName = document.createAttribute("name");
+        noName.value = "readSelect";
+        readNo.setAttributeNode(noName);
+        const noValue = document.createAttribute("value");
+        noValue.value = "no";
+        readNo.setAttributeNode(noValue);
+        readNo.dataset.radio = "no";
+        readNo.innerText = "no";
+        para.appendChild(readNo);
+
         var readlabel = document.createElement('label');  
         const labelclass = document.createAttribute("class");
-        labelclass.value = "label";
+        labelclass.value = "noClass";
         readlabel.setAttributeNode(labelclass);
-        readlabel.innerHTML = "Read this book?"
-        box.appendChild(readlabel);
-    
-        var readselect = document.createElement('select');
-        const selectclass = document.createAttribute("class");
-        selectclass.value = ("select");
-        readselect.setAttributeNode(selectclass);
-        readlabel.appendChild(readselect);
-    
-        var readoptionno = document.createElement("option");
-        const optionclassno = document.createAttribute("value");
-        readoptionno.setAttributeNode(optionclassno);
-        readoptionno.value = 0;
-        readoptionno.innerHTML = "No";
-        readselect.appendChild(readoptionno);
-    
-        var readoptionyes = document.createElement("option");
-        const optionclassyes = document.createAttribute("value");
-        readoptionyes.setAttributeNode(optionclassyes);
-        readoptionyes.value = 1;
-        readoptionyes.innerHTML = "Yes";
-        readselect.appendChild(readoptionyes);
+        const labelFor = document.createAttribute("for");
+        labelFor.value = "readSelect";
+        readlabel.setAttributeNode(labelFor);
+        readlabel.innerHTML = "No";
+        para.appendChild(readlabel);
 
-        //logic behind targeting the selector icon and changing the book
-        Array.from(document.getElementsByClassName("select")).forEach(function(element) {
-          element.addEventListener('click', () => {
-              element.addEventListener("change", () => {
-                console.log(indexNum);
-                if (element.value == 1) {
-                  console.log(indexNum);
-                  //yes, update book display
-                  //--bug first book acts like parent to next.
-                  //--first book then controls all children.
-                  info.innerHTML = `Title \xa0: \xa0\xa0 ${newBook[0]}` + "<br>" + `Author \xa0:\xa0\xa0 ${newBook[1]}` + "<br>" + `Pages \xa0:\xa0\xa0 ${newBook[2]}`
-                    + "<br>" + `Published \xa0:\xa0\xa0  ${newBook[3]}` + "<br>" + `Date Read \xa0:\xa0\xa0 ${newBook[5]}`;
-                  li.setAttributeNode(classAttribute1);
-                }
-                else {
-                  console.log("duck");
-                  //no, update display
-                  info.innerHTML = `Title \xa0: \xa0\xa0 ${newBook[0]}` + "<br>" + `Author \xa0:\xa0\xa0 ${newBook[1]}` + "<br>" + `Pages \xa0:\xa0\xa0 ${newBook[2]}`
-                    + "<br>" + `Published \xa0:\xa0\xa0  ${newBook[3]}`;
-                  //need change to this element not all books
-                  li.setAttributeNode(classAttribute);
-                }
-              });
-            })
-          });       
-};        
-      
-
-
-
+   //  logic behind targeting the selector icon and changing the book
+ Array.from(document.getElementsByClassName("radioClass")).forEach(function(element) {
+  element.addEventListener('click', () => {
+      element.addEventListener("change", () => {
+     if (element.value == "Yes") {
+          console.log(element.parentElement.parentElement.parentElement)
+             info.innerHTML = `Title \xa0: \xa0\xa0 ${newBook[0]}` + "<br>" + `Author \xa0:\xa0\xa0 ${newBook[1]}` + "<br>" + `Pages \xa0:\xa0\xa0 ${newBook[2]}`
+            + "<br>" + `Published \xa0:\xa0\xa0  ${newBook[3]}` + "<br>" + `Date Read \xa0:\xa0\xa0 ${newBook[5]}`;
+            element.parentElement.parentElement.parentElement.setAttributeNode(classAttribute1);
+        }
+        else {
+          console.log("duck");
+          //no, update display
+          info.innerHTML = `Title \xa0: \xa0\xa0 ${newBook[0]}` + "<br>" + `Author \xa0:\xa0\xa0 ${newBook[1]}` + "<br>" + `Pages \xa0:\xa0\xa0 ${newBook[2]}`
+            + "<br>" + `Published \xa0:\xa0\xa0  ${newBook[3]}`;
+          //need change to this element not all books
+          li.setAttributeNode(classAttribute);
+        }
+    })
+  })    
+})
+}
+              
 // logic behind submit book button
 const formSubmit = function (event) {
-  //this little bugger had me stuck for too long!! im sure it disabled my button before.
   event.preventDefault();
   //declare my values in the function for my eventlistener submit button
   title = this.title.value;
@@ -154,6 +190,8 @@ const formSubmit = function (event) {
   submitArray.push(userBook.info());
   toggleForm();
   addBookToLibrary();
+  console.log(myLibrary)
+  console.log(submitArray)
 };
 
 //logic behind add new book/get form button
@@ -174,22 +212,18 @@ function toggleForm() {
 
 //more of a default display function for now. 
 function buildLibary() {
-  if(myLibrary == 0) {
+  if(myLibrary.length === 0 ) {
   emptyBook.innerHTML = "No books found, add books!"
+  console.log(myLibrary.length)
   }
   else {
-
+    console.log(myLibrary.length)
   }
 };
 
 window.onLoad = buildLibary();
 
-
-
 // upload new book button
 form.addEventListener("submit", formSubmit);
 //add new book button
 getForm.addEventListener('click', toggleForm);
-
-
-
